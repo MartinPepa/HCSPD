50f032d034527deed462ad21c4eec56fc876339b
MartinPepa <89148337+MartinPepa@users.noreply.github.com>
2023-03-21 00:37:01 -0300

    Incorporación de gráficas

--- M1.py
+++ M1.py
@@ -5,18 +5,16 @@ Created on Sun Mar 19 19:18:19 2023
 """
 
 #%%
+
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy
-
-#%%
 
 # Ejercicio 1a
 t       = np.linspace(0, 20, num=201)
 ten     = 12 - 8 * np.sqrt(3) * np.exp(-t/2) * np.sin( np.sqrt(3) * t/2 + np.pi/3 )
 corr    = 8 * np.sqrt(3) * np.exp(-t/2) * np.sin( np.sqrt(3)*t/2 )
 
-fig, ax = plt.subplots(dpi=300)
+fig, ax = plt.subplots(dpi=400)
 ax.plot(t,ten)
 ax.set_xlim(0.0,20.0)
 ax.set_ylim(0.0,15.0)
@@ -24,8 +22,9 @@ ax.set_title('Tensión (voltaje) del capacitor')
 ax.set_xlabel('t [s]')
 ax.set_ylabel('Vc [V]')
 plt.grid(visible = True, which = 'both', axis = 'both')
+plt.savefig('Ejercicio-1a_tension.png', dpi=400, format='png', orientation='landscape')
 
-fig, bx = plt.subplots(dpi=300)
+fig, bx = plt.subplots(dpi=400)
 bx.plot(t,corr)
 bx.set_xlim(0.0,20.0)
 bx.set_ylim(-2.0,7.0)
@@ -33,6 +32,7 @@ bx.set_title('Corriente')
 bx.set_xlabel('t [s]')
 bx.set_ylabel('I [A]')
 plt.grid(visible = True, which = 'both', axis = 'both')
+plt.savefig('Ejercicio-1a_corriente.png', dpi=400, format='png', orientation='landscape')
 plt.show()
 
 
@@ -43,6 +43,10 @@ plt.show()
 
 # Ejercicio 2
 # ATENCIÓN: PENDIENTE DE CHEQUEAR
+
+import numpy as np
+import matplotlib.pyplot as plt
+
 '''
 Las soluciones analíticas son
 
@@ -56,14 +60,14 @@ Las soluciones analíticas son
     + 0.00545477086295742*exp(-4545.5484636724*t)
 '''
     
-t       = np.linspace(0, 0.1,num=100)
-ten     = 12 - 0.00024794925315873*np.exp(-219995454.451536*t)\
+t       = np.linspace(0, 0.005,num=101)
+ten     = 12 - 0.000247949253158732*np.exp(-219995454.451536*t)\
     -12.0002479492532*np.exp(-4545.5484636724*t)
 corr    = 8.67361737988404e-19\
     - 0.00545477086295743*np.exp(-219995454.451536*t)\
     + 0.00545477086295742*np.exp(-4545.5484636724*t)
 
-fig, ax = plt.subplots(dpi=300)
+fig, ax = plt.subplots(dpi=400)
 ax.plot(t,ten)
 #ax.set_xlim(0.0,20.0)
 #ax.set_ylim(0.0,15.0)
@@ -71,8 +75,9 @@ ax.set_title('Tensión (voltaje) del capacitor')
 ax.set_xlabel('t [s]')
 ax.set_ylabel('Vc [V]')
 plt.grid(visible = True, which = 'both', axis = 'both')
+plt.savefig('Ejercicio-2_tension.png', dpi=400, format='png', orientation='landscape')
 
-fig, bx = plt.subplots(dpi=300)
+fig, bx = plt.subplots(dpi=400)
 bx.plot(t,corr)
 #bx.set_xlim(0.0,20.0)
 #bx.set_ylim(-2.0,7.0)
@@ -80,6 +85,7 @@ bx.set_title('Corriente')
 bx.set_xlabel('t [s]')
 bx.set_ylabel('I [A]')
 plt.grid(visible = True, which = 'both', axis = 'both')
+plt.savefig('Ejercicio-2_corriente.png', dpi=400, format='png', orientation='landscape')
 plt.show()
 
 #%%
